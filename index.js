ec7010848ffadf6c4c81b1f24540566ac5b701d1
markke0505 <markke0505@googlemail.com>
2017-05-11 04:07:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ restService.use(bodyParser.urlencoded({
 }));
 
 restService.use(bodyParser.json());
-/*
+
 restService.post('/ask-character', function(req, res) {
     var speech = req.body.result.parameters.character
     var answer
     
-    if (character == "homer simpson"){
+    if (speech == "homer simpson"){
         answer = "He is the big daddy"
     }
     else{
@@ -28,8 +28,8 @@ restService.post('/ask-character', function(req, res) {
         source: 'ask-character-source'
     });
 });
-*/
 
+/*
 restService.post('/google-action-test', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
@@ -38,7 +38,7 @@ restService.post('/google-action-test', function(req, res) {
         source: 'google-action-test-source'
     });
 });
-
+*/
 
 /*
 restService.post('/slack-test', function(req, res) {
